28dd4c0344fd5109d5c9d90dfd07d5d2b7924afd
giapnv-2221 <nguyen.van.giap-b@sun-asterisk.com>
2025-04-03 23:34:01 +0700

    Bashscript to install

---
+++ config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script để nhập và export các biến môi trường
+
+# Nhập giá trị cho biến CLOUDFLARE_TUNNEL_TOKEN
+read -p "Nhập Cloudflare Tunnel Token (CLOUDFLARE_TUNNEL_TOKEN): " CLOUDFLARE_TUNNEL_TOKEN
+
+# Nhập giá trị cho biến EXTERNAL_IP
+read -p "Nhập Domain/External IP cho n8n callback url(Ví dụ: https://n8n.giapnv.io.com): " EXTERNAL_IP
+
+# Export các biến
+export CLOUDFLARE_TUNNEL_TOKEN="$CLOUDFLARE_TUNNEL_TOKEN"
+export EXTERNAL_IP="$EXTERNAL_IP"
+
+# Hiển thị thông báo thành công
+echo "Đã export các biến môi trường"
+
+# Ghi các biến vào file .env
+cat > .env <<EOF
+# Cloudflare
+CLOUDFLARE_TUNNEL_TOKEN=$CLOUDFLARE_TUNNEL_TOKEN
+
+# n8n Configuration
+EXTERNAL_IP=$EXTERNAL_IP
+EOF
+
+# Hiển thị thông báo thành công
+echo "Đã lưu các biến vào file .env:"
+cat .env
+
---
+++ install_for_root.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "--------- 🟢 Start install docker -----------"
+# Add Docker's official GPG key:
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+echo "--------- 🔴 Finish install docker -----------"
+echo "--------- 🟢 Start creating folder -----------"
+cd ~
+mkdir n8n/vol_n8n
+cd n8n
+chown -R 1000:1000 vol_n8n
+chmod -R 755 vol_n8n
+echo "--------- 🔴 Finish creating folder -----------"
+echo "--------- 🟢 Start docker compose up  -----------"
+apt install wget -y
+wget https://raw.githubusercontent.com/haokiet97/n8n_config/refs/heads/main/compose.yml -O compose.yaml
+echo "--------- 🔴 Finish! Let's start run_n8n.sh to start -----------"
