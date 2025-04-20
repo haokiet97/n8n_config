d88b4ed22b7cd5b653db4c64240bad0327cf6b6f
haokiet97 <33271409+haokiet97@users.noreply.github.com>
2025-04-20 22:28:05 +0700

    Update start_recording.sh

--- record_camera/start_recording.sh
+++ record_camera/start_recording.sh
@@ -53,10 +53,6 @@ start_ffmpeg() {
         ffmpeg \
             -loglevel error \
             -rtsp_transport tcp \
-            -reconnect 1 \
-            -reconnect_at_eof 1 \
-            -reconnect_streamed 1 \
-            -reconnect_delay_max 5 \
             -i "$uri" \
             -c copy \
             -f segment \
